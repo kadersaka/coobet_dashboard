e3810806fd0fc012805840f4ea16e5b0fb9cf103
NabilWafick <nabilsoumanou7@gmail.com>
2024-12-24 20:56:11 +0100

    fix ' issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <div className="text-center">
         <h1 className="mb-4 text-6xl font-bold">404</h1>
         <h2 className="mb-2 text-2xl font-semibold">Page Non trouvée</h2>
-        <p className="mb-6 ">Oops! La page recherchée n'existe pas.</p>
+        <p className="mb-6 ">Oops! La page recherchée n&apos;existe pas.</p>
         <Link href="/">
           <span className="text-lg font-medium text-primary hover:underline">
             Page d'accueil
